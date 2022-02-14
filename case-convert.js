94354784d0b9c95710d4d9632c3808bd7d81feec
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-14 08:55:38 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,13 @@
+//searching
+//toUpperCase()
+//toLowerCase()
+const anthem = "amar Sonar Bangla Ami Tomai Valobashi";
+const search = "valo";
+
+const userInput = "blACkPink";
+const savedUserName = "blackPink";
+if(userInput.toLowerCase() == savedUserName.toLowerCase()) {//it will make all latter in lower case. you should use lower case method while comparing two string.
+//or can be convert to upperCase.
+    console.log("user exists");
+}
+
